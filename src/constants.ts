e013a3f2e0ac87ebee22c6478a9be1436b43cd2e
janaryalfaro3-lab <janaryalfaro3@gmail.com>
2026-05-08 09:55:41 +0800

    feat: Update hero video and fallback logo
    Replaces the placeholder hero video with the actual `hero-video.mp4`.
    Updates the fallback logo URL to point to the correct resource.

--- src/constants.ts
+++ src/constants.ts
@@ -5,10 +5,10 @@
  */
 
 export const LOGO_URL = "/logo.jpg";
-export const FALLBACK_LOGO_URL = "https://scontent.fmnl17-5.fna.fbcdn.net/v/t39.30808-6/472457306_122121870752610387_8965074152194860895_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=1d70fc&_nc_ohc=NU7dA5a41-UQ7kNvwGjqcLi&_nc_oc=AdrdPK2YxeYoUQ87_varUb-Ap6rhyzGDKRrvk6xuFAST5GZiH-34Clccuz2IWCuNBE8&_nc_zt=23&_nc_ht=scontent.fmnl17-5.fna&_nc_gid=kY1Y9yV-LJyNaeGCDm5rNg&_nc_ss=7b2a8&oh=00_Af3avINw-4p1hErGNznFL5tWIeJT8dCNDC7FEvAMqCAqDg&oe=69F65804";
+export const FALLBACK_LOGO_URL = "https://scontent.fmnl17-5.fna.fbcdn.net/v/t39.30808-6/472457306_122121870752610387_8965074152194860895_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=1d70fc&_nc_eui2=AeGwQK6_9pQ9nSjqUIuMR2mvLAntN-fDVnUsCe0358NWdcj81TaU5EYn5uCiUAbul1McnzPj7gY9L3JZDGGh8ucq&_nc_ohc=ne9QlyImuZ8Q7kNvwF4pDuM&_nc_oc=AdobsQ-SPQX2YPLuVt_p173Mvo4cSJFxJS3OA2-ElQaGudGsqdyMZ3OMdc26bikzXs4&_nc_zt=23&_nc_ht=scontent.fmnl17-5.fna&_nc_gid=n208daL-7ACyxB566aqHgQ&_nc_ss=7b2a8&oh=00_Af5s7QQU-fB60DoQx2HQgJteaTUK6H7VkOCoJWx0lpaDxw&oe=6A031684";
 
 export const VIDEO_HERO = {
-  local: "/202605021041.mp4",
+  local: "/hero-video.mp4",
   remote: "https://raw.githubusercontent.com/janaryalfaro3-lab/canvas-of-dreams/main/public/202605021041.mp4"
 };
 
